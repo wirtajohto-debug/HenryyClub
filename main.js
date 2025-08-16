307a4e3b991f84c8890c961c02ffd82c7ece0ed0
wirtajohto-debug <wirtajohto@gmail.com>
2025-08-16 21:07:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Firebase config (placeholder)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAGqaifr-68EzB85IvHGHMo7af3FAERryo",
+  authDomain: "henryclub-383e5.firebaseapp.com",
+  projectId: "henryclub-383e5",
+  storageBucket: "henryclub-383e5.firebasestorage.app",
+  messagingSenderId: "972447053262",
+  appId: "1:972447053262:web:008a5a04c7a17e2645279f",
 };
 
 firebase.initializeApp(firebaseConfig);
